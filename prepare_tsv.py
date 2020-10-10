3bb0636f61ae8d5cedca2782369e295ce337741f
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2020-10-10 16:49:26 -0700

    Remove header from data

--- prepare_tsv.py
+++ prepare_tsv.py
@@ -20,5 +20,5 @@ full_embedding = np.array(list(data.vector.values))
 print(full_embedding.shape)
 
 # data[['title', 'published_at']].to_csv('data.tsv', sep='\t', index=False)
-data[['title']].to_csv('data.tsv', sep='\t', index=False)
+data[['title']].to_csv('data.tsv', sep='\t', index=False, header=None)
 np.savetxt('embeddings.tsv', full_embedding, '%.6f', delimiter='\t')
